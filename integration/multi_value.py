fc3d4a87ae982516e53a3e2847362ac771e466b0
lemming52 <andrew@gumboot.co.uk>
2016-02-05 01:44:02 +0000

    Correct Multi_value and add plotting.
    Correct multi_value.py to corectly store the data and add a plotting
    script to produce two charts as required.

--- integration/multi_value.py
+++ integration/multi_value.py
@@ -8,14 +8,15 @@ import numpy as np
 def main():
     steps = 14
     with open('samples.dat', 'w') as f:
-        f.write('Samples\tValue\tError')
+        f.write('Samples\tValue\tError\n')
     f.close()
     for i in range(steps):
-        samples = np.power(10, i/2)
-        value, error = step(samples)
-        with open('samples.dat', 'a') as f:
-            f.write('%s\t%s\t%s' % (samples, value, error, ))
-        f.close()
+        samples = int(np.rint(np.power(10, (i+1)/2)))
+        print(samples)
+        #value, error = step(samples)
+        #with open('samples.dat', 'a') as f:
+        #    f.write('%s\t%s\t%s\n' % (samples, value, error))
+        #f.close()
 
 
 def step(samples):
