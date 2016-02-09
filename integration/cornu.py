337558f0f1ebaa13a2cd682fa3ccd683859876b9
lemming52 <andrew@gumboot.co.uk>
2016-02-09 12:22:25 +0000

    Change fresnel to a package.
    May have been unnecesary, but i had issues using the integrals for
    diffraction with the cornu code being run.

---
+++ integration/cornu.py
@@ -0,0 +1,25 @@
+import fresnel
+
+# External Packages
+import numpy as np
+from scipy.integrate import quad
+
+
+def coord_pair(u):
+    coeff = np.pi/2
+    C = quad(fresnel.cos_integrand, 0, u, args=(coeff, ))
+    S = quad(fresnel.sin_integrand, 0, u, args=(coeff, ))
+    return C[0], S[0]
+
+
+def cornu():
+    u_range = np.linspace(-10, 10, 1000)
+    with open('cornu.dat', 'w') as f:
+        f.write('u\tC\tS\n')
+        for u in u_range:
+            C, S = coord_pair(u)
+            f.write('%s\t%s\t%s\n' % (u, C, S))
+    f.close()
+
+
+cornu()
