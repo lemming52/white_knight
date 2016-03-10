9216a86259e55b62a456d28da98b950c7f48c1dc
lemming52 <andrew@gumboot.co.uk>
2016-03-10 17:14:31 +0000

    Finish diffraction script.
    Correct to produce the actual pattern properly. Adjust methods
    to support the new config files to allow one command regeneration.
    Refactor methods for generating aperture functions for speed. Add
    correct plotting methods.

--- diffraction/diffraction.py
+++ diffraction/diffraction.py
@@ -6,65 +6,107 @@ import numpy as np
 import tools
 
 
-def aperture_1d(length):
+def gen_aperture_1d(length):
     return np.array(np.zeros(length), dtype=complex)
 
 
-def slit_1d(aperture, half_width, centre, conversion, ap_func, *args):
-    slit_min = centre - half_width
-    slit_max = centre + half_width
-    print(slit_min+slit_max)
-    ap = np.array([ap_func(i, half_width, args) for i in range(slit_max - slit_min)])
-    aperture[slit_min: slit_max] = ap
+def aperture_1d(aperture, half_width, centre, conversion, ap_func, args):
+    ap_min = centre - half_width
+    ap_max = centre + half_width
+    i = np.array(range(ap_max-ap_min))
+
+    aperture[ap_min: ap_max] = ap_func(i, half_width, conversion, args)
     return aperture
 
 
-def hole(i, width, *args):
+def slit(i, half_width, *args):
     return complex(1, 0)
 
 
-def grating(i, width, conversion, args):
+def grating(i, half_width, conversion, args):
     s = args['s']
     m = args['m']
-    return complex(1, m/2*np.sin(2*np.pi*(i-width)*conversion/s))
+    x = (i-half_width)*conversion
+    phase = np.exp(1j*m*np.sin(2*np.pi*x/s)/2)
+    return phase
 
 
 def evaluate(slit, ap_func, correction, label):
-    aperture = aperture_1d(262144)
 
-    conversion = slit['extent'] / 262144
-    half_width = round(slit['width'] /(2*conversion))
-    centre = round(slit['centre'] / conversion)
+    N = 2097152
+
+    aperture = gen_aperture_1d(N)
+
+    conversion = slit['extent'] / N
+
+    half_width = convert(slit['width']/2, conversion)
+    centre = convert(slit['centre'], conversion)
+
     distance = slit['distance']
-    args = slit['args']
     wavelength = slit['wavelength']
-    aperture = slit_1d(aperture, half_width, centre, conversion, ap_func, args)
+    args = slit['args']
+    aperture = aperture_1d(aperture, half_width, centre,
+                           conversion, ap_func, args)
+
     if correction:
-        aperture = correct(aperture, wavelength, distance)
+        aperture = correct(aperture, wavelength, distance, conversion)
+
     amplitude = conversion*np.fft.fft(aperture)
-    y = distance*wavelength/(2*np.pi)*np.fft.fftfreq(amplitude.size, d=conversion)
-    plot(amplitude, y, slit, conversion)
+    amplitude = np.fft.fftshift(amplitude)
+
+    # No 2pi as fft.freq gives regular as opposed to angular freq.
+    y = distance*wavelength*np.fft.fftfreq(amplitude.size,
+                                           d=conversion)
+    y = np.fft.fftshift(y)
+
+    plot(amplitude, y, slit, label)
+
+
+def convert(value, conversion):
+    return round(value / conversion)
+
+
+def correct(aperture, wavelength, distance, conversion):
+    i = np.array(range(aperture.size))
+    x = (i-len(aperture)/2)*conversion
+    correction = np.exp(1j*np.pi*np.power(x, 2)/(distance*wavelength))
+    return correction*aperture
 
 
 def get_func(name):
-    if name == 'hole':
-        return hole
+    if name == 'slit':
+        return slit
     elif name == 'grating':
         return grating
     else:
         print("I don't know that function")
 
 
-def plot(amplitude, y, slit, conversion):
-    factor = slit['width']*np.pi*y/(slit['wavelength']*slit['distance'])
-    predicted = conversion*np.power(np.sin(factor)/factor, 2)
-    plt.figure()
-    intensity = np.power(abs(amplitude), 2)
-    plt.plot(y, intensity, color='r')
-    plt.plot(y, predicted)
-    plt.xlim(-100*slit['width'], 100*slit['width'])
-    plt.show()
+def plot(amplitude, y, slit, label):
 
+    intensity = np.power(abs(amplitude), 2)
+    intensity = intensity/np.amax(intensity)
+
+    plt.figure()
+
+    if slit['predicted']:
+        plt.plot(y, predicted(y, slit), label='Predicted')
+    plt.plot(y, intensity, color='r', label='Calculated')
+
+    plt.xlim(-slit['lim'], slit['lim'])
+    plt.ylim(0, 1.1)
+    plt.xlabel('y / m')
+    plt.ylabel('Fractional Intensity compared to Maximum')
+    plt.title('Diffaction Pattern: %s, D=%sm' % (slit['ap_func'],
+                                                 slit['distance']))
+    plt.legend()
+    plt.savefig('pattern_%s.png' % label)
+
+
+def predicted(y, slit):
+    factor = slit['width']*y/(slit['wavelength']*slit['distance'])
+    predicted = np.power(np.sinc(factor), 2)
+    return predicted
 
 def main():
     label = input('Enter config file label: ')
